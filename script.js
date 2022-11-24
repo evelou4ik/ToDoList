1131357c4e812e095b8f05bef7bb4d7889b27c3d
evel0u4ik <yevseev.dima@gmail.com>
2022-11-24 19:02:05 +0200

    First final version (without filter of date)

--- script.js
+++ script.js
@@ -1,59 +1,114 @@
-const inputText = document.querySelector("input[type='text']");
-const btnAdd = document.querySelector(".btn_create");
-const checkAll = document.querySelector("input[type='checkbox']");
-const listOfItems = document.querySelector("ul");
+const inputTitle = document.querySelector(".input_title");
+const btnCreate = document.querySelector('.btn_create');
+const btnSort = document.querySelector(".btn_sort");
+const checkAll = document.querySelector(".check_all");
+const checkComplete = document.querySelector(".check_complete");
+const checkUnComplete = document.querySelector(".check_uncomplete");
+const listOfItems = document.querySelector('ul');
+
 
 function createToDoItem(value) {
-  let item = document.createElement('LI');
-  let itemContainer = document.createElement('DIV');
-  item.setAttribute("data-status", "active")
-  itemContainer.classList.add("item_wrap")
-  itemContainer.innerHTML =
-      `
-  <div class="item_descr">
-    <span>${value}</span>
-    <span></span>
-  </div>
-  <div class='item_wrap_btns'>
-    <button class='btn btn_complete'>Выполнено</button> 
-    <button class='btn btn_delete'>Удалить</button> 
-  </div>
-  `
-  item.append(itemContainer)
-  listOfItems.append(item);
+    const item = document.createElement("LI");
+    item.setAttribute("data-status", "active");
+    item.classList.add("item")
+    item.innerHTML =
+        `
+    <div class="item_content">
+        <span class="title">${value}</span>
+        <span class="time"></span>
+    </div>
+    </div class="item_wrap_btns">
+        <button class="btn btn_complete">Выполнено</button>
+        <button class="btn btn_delete">Удалить</button>
+    </div>
+    `
+    listOfItems.append(item);
 }
 
-btnAdd.addEventListener("click", () => {
-  if (inputText.value !== "") {
-    createToDoItem(inputText.value)
-  }
+
+btnCreate.addEventListener('click', () => {
+    const inputTitleValue = (inputTitle.value).trim();
+    if (inputTitleValue !== "" && inputTitleValue) {
+        createToDoItem(inputTitleValue);
+    }
 })
 
-listOfItems.addEventListener("click", (e) => {
-  let target = e.target;
-  let targetLi = target.closest("li");
+listOfItems.addEventListener('click', (e) => {
+    const target = e.target;
+    let targetLi = target.closest(".item");
 
-  if (target.closest(".btn_complete")) {
-    targetLi.setAttribute("data-status", "complete");
-  }
+    if (target.classList.contains("btn_complete")) {
+        const date = new Date();
+        const timeOfCreate = date.toLocaleString();
+        if (targetLi.getAttribute("data-status") !== "complete") {
+            targetLi.setAttribute("data-status", "complete");
+            targetLi.setAttribute("data-time", `${date}`)
+            targetLi.querySelector(".time").innerHTML = timeOfCreate;
+            targetLi.classList.add("hidden");
+        } else {
+            targetLi.setAttribute("data-status", "active");
+            targetLi.removeAttribute("data-time");
+            targetLi.querySelector(".time").innerHTML = "";
+            targetLi.classList.remove("hidden");
+        }
+    }
 
-  if (target.closest(".btn_delete")) {
-    targetLi.remove()
-  }
+    if (target.classList.contains("btn_delete")) {
+        targetLi.remove();
+    }
+})
+
+checkComplete.addEventListener('click', () => {
+    if (checkComplete.checked) {
+        for (const item of document.querySelectorAll(".item")) {
+            item.classList.remove("hidden")
+            if (item.getAttribute("data-status") !== "complete") {
+                item.classList.add("hidden")
+            } else {
+                item.classList.remove("hidden")
+            }
+        }
+    } else {
+        for (const item of document.querySelectorAll(".item")) {
+            item.classList.remove("hidden")
+            if (item.getAttribute("data-status") !== "active") {
+                item.classList.add("hidden")
+            }
+        }
+    }
+})
+
+checkUnComplete.addEventListener('click', () => {
+    if (checkUnComplete.checked) {
+        for (const item of document.querySelectorAll(".item")) {
+            item.classList.remove("hidden")
+            if (item.getAttribute("data-status") !== "active") {
+                item.classList.add("hidden")
+            }
+        }
+    }
 })
 
 checkAll.addEventListener('click', () => {
-  const toDoItems = document.querySelectorAll("li")
-  toDoItems.forEach(el => {
-    if (el.getAttribute("data-status") === 'completed') {
-      console.log(el)
+    if (checkAll.checked) {
+        for (const item of document.querySelectorAll(".item")) {
+            item.classList.remove("hidden")
+        }
+    } else {
+        for (const item of document.querySelectorAll(".item")) {
+            if (item.getAttribute("data-status") === 'complete') {
+                item.classList.add("hidden")
+            }
+        }
     }
-  })
-  if (checkAll.checked && toDoItems.length !== 0) {
+})
+
+btnSort.addEventListener('click', () => {
+    const completeItems = document.querySelectorAll('.item[data-status="complete"]');
+    const filterTimeArr = []
+    for (const item of completeItems) {
+        filterTimeArr.push(item.outerHTML);
+    }
 
 
-    // const completedItems = toDoItems.filter(el => )
-    // console.log(completedItems)
-
-  }
 })
